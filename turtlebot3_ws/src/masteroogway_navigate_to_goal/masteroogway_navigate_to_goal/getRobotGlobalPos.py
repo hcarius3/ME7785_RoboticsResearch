144ee5ba20d02da226747a1a7fe9b4c58aaca573
henic <hcarius3@gatech.edu>
2025-03-01 02:45:49 -0500

    Adapted to our ROS communication. Aditionaly normalized the orientation angle to always be between -pi and pi

--- turtlebot3_ws/src/masteroogway_navigate_to_goal/masteroogway_navigate_to_goal/getRobotGlobalPos.py
+++ turtlebot3_ws/src/masteroogway_navigate_to_goal/masteroogway_navigate_to_goal/getRobotGlobalPos.py
@@ -3,25 +3,27 @@
 
 import rclpy
 from rclpy.node import Node
+from geometry_msgs.msg import Point,Pose
 from nav_msgs.msg import Odometry
 import numpy as np
 
 class getRobotGlobalPos(Node):
     def __init__(self):
         super().__init__('odom_listener')
-        self.subscription = self.create_subscription(
-            Odometry,
-            '/odom',
-            self.update_Odometry,
-            10
-        )
-        self.subscription  # Prevent unused variable warning
+
+        # Subscribe to odom message
+        self.subscription = self.create_subscription(Odometry, '/odom', self.update_Odometry, 10)
+        
+        # Publish the global position of the robot
+        self.publisher = self.create_publisher(Pose, '/rob_pose', 10)
         
         # Initialization variables
         self.Init = True
         self.Init_ang = 0.0
-        self.Init_pos = np.zeros(3)
-        self.globalPos = np.zeros(3)
+        self.Init_pos = Point()
+        self.Init_pos.x = 0.0
+        self.Init_pos.y = 0.0
+        self.globalPos = self.Init_pos
         self.globalAng = 0.0        
 
     def update_Odometry(self, Odom):
@@ -36,25 +38,22 @@ class getRobotGlobalPos(Node):
             self.Init_ang = orientation
             self.globalAng = self.Init_ang
             self.Mrot = np.matrix([[np.cos(self.Init_ang), np.sin(self.Init_ang)], [-np.sin(self.Init_ang), np.cos(self.Init_ang)]])        
-            self.Init_pos = np.array([
-                self.Mrot.item((0,0))*position.x + self.Mrot.item((0,1))*position.y,
-                self.Mrot.item((1,0))*position.x + self.Mrot.item((1,1))*position.y,
-                position.z
-            ])
+            self.Init_pos.x = self.Mrot.item((0,0))*position.x + self.Mrot.item((0,1))*position.y
+            self.Init_pos.y = self.Mrot.item((1,0))*position.x + self.Mrot.item((1,1))*position.y
+            self.Init_pos.z = position.z
         
         # self.Mrot = np.matrix([[np.cos(self.Init_ang), np.sin(self.Init_ang)], [-np.sin(self.Init_ang), np.cos(self.Init_ang)]])        
         
         # Apply the transformation to correct odometry
-        self.globalPos = np.array([
-            self.Mrot.item((0,0))*position.x + self.Mrot.item((0,1))*position.y - self.Init_pos[0],
-            self.Mrot.item((1,0))*position.x + self.Mrot.item((1,1))*position.y - self.Init_pos[1],
-            position.z - self.Init_pos[2]
-        ])
+        self.globalPos.x = self.Mrot.item((0,0))*position.x + self.Mrot.item((0,1))*position.y - self.Init_pos.x
+        self.globalPos.y = self.Mrot.item((1,0))*position.x + self.Mrot.item((1,1))*position.y - self.Init_pos.y
+        # self.globalPos.z = position.z - self.Init_pos.z
         self.globalAng = orientation - self.Init_ang
 
-        self.get_logger().info(f'Corrected Position: ({self.globalPos[0]:.2f}, {self.globalPos[1]:.2f}), Yaw: {self.globalAng:.2f} rad')
-        
+        # Normalize angle to range [-pi, pi]
+        self.globalAng = (self.globalAng + np.pi) % (2 * np.pi) - np.pi
 
+        self.get_logger().info(f'Corrected Position: ({self.globalPos.x:.2f}, {self.globalPos.y:.2f})m, Yaw: {self.globalAng:.2f}rad')
 
 def main():
 	rclpy.init() # init routine needed for ROS2.
